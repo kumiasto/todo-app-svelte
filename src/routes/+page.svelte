b32059ab8d99c5f83dce424e391938f15d4e0048
Kamil Umiastowski <kumiasto@gmail.com>
2024-03-12 19:17:14 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+
+<script>
+	import '/src/styles/global.css';
+</script>
+
+<main class="container">
+   <section>siema</section>
+   <section class="input-wrapper">
+      <div >
+         <label for="elo">Add new todo</label>
+         <input id="elo"/>
+      </div>
+   </section>
+</main>
+
+
+<style>
+   .container{
+      height: 100vh;
+   }
+
+   .container section { 
+      height: 50%;
+   }
+
+   .input-wrapper div {
+      background-color: green;
+      display: flex;
+      flex-direction: column;
+      justify-items: end;
+   }
+</style>
